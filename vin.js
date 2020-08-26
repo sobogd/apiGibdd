ef91252596a001f3e5886362feca8a49c25691c9
sobogd <sobogd@gmail.com>
2020-08-26 11:00:18 +0300

    Доработка под введение разных типов данных из командной строки

--- vin.js
+++ vin.js
@@ -58,8 +58,16 @@ const sleep = (ms) => {
 }
 
 (async () => {
+    // проверка наличия вина
+    if(!process.argv[2] || process.argv[2] === ''){
+        return console.log(JSON.stringify({error:'Не указан VIN!'}));
+    }
+    // проверка на заполнение типа информации - dtp/reg/ogr/roz
+    if(process.argv[3] !== 'dtp' && process.argv[3] !== 'reg' && process.argv[3] !== 'ogr' && process.argv[3] !== 'roz'){
+        return console.log(JSON.stringify({error:'Не указан тип необходимой информации!'}));
+    }
     // создаем объект для дальнейшей записи в него
-    let returnObject = { vehicle: {}, ownershipPeriods: [] }
+    let returnObject = { vehicle: {}, ownershipPeriods: [] }, id
     // подготавливаем страницу
     const browser = await puppeteer.launch({
         args: ['--no-sandbox'],
@@ -75,33 +83,42 @@ const sleep = (ms) => {
     await sleep(1000)
     await page.click('.bs-holder2 ul li:nth-child(2) a');
     await page.goto('https://xn--90adear.xn--p1ai/check/auto');
-    await sleep(1000)
+    await sleep(4000)
     // вводим ВИН по символу имитируя пользователя
     let vin = process.argv[2]
     for (n = 0; n < vin.length; n++) {
         await sleep(200);
         await page.type('#checkAutoVIN', vin[n]);
     }
+    // в зависимоти от нужного типа инфы указываем айдишник
+    if(process.argv[3]==='reg')id='checkAutoHistory'
+    if(process.argv[3]==='dtp')id='checkAutoAiusdtp'
+    if(process.argv[3]==='ogr')id='checkAutoRestricted'
+    if(process.argv[3]==='roz')id='checkAutoWanted'
     // кликаем по кнопке запросить ИСТОРИЮ РЕГИСТРАЦИИ
-    await page.click('#checkAutoHistory a');
+    await page.click('#'+id+' a');
     // ждем что выдаст сервер
     let i = 0
-    while (i <= 40) {
-        let html = await page.$eval('#checkAutoHistory', e => e.innerHTML);
-        if (html.indexOf('Выполняется запрос, ждите') + 1 && !html.indexOf('<ul class="fields-list vehicle">') + 1) {
+    while (i <= 60) {
+        let html = await page.$eval('#'+id+'', e => e.innerHTML);
+        if (html.indexOf('Выполняется запрос, ждите') + 1 && !html.indexOf('<ul class="fields-list">') + 1) {
             //console.log('Ожидание')
         } else {
-            i = 45
+            i = 65
         }
         await sleep(2000);
         i++
     }
+    // ошибка если так и не загрузилось
+    if(i <= 64){
+        return console.log(JSON.stringify({error:'Слишком долгое время ожидания с сайта ГИБДД!'}));
+    }
     // получаем содержимое html блока истории регистрации авто
-    let checkAutoHistory = await page.$eval('#checkAutoHistory', e => e.innerHTML);
+    let checkAutoHistory = await page.$eval('#'+id+'', e => e.innerHTML);
     // парсим дом
     checkAutoHistory = cheerio.load(checkAutoHistory);
     // проходим по свойствам авто
-    checkAutoHistory('ul.fields-list.vehicle li').each(function (i, elem) {
+    checkAutoHistory('ul.fields-list li').each(function (i, elem) {
         returnObject.vehicle[checkAutoHistory(this).children().first().text().replace(':', '')] = checkAutoHistory(this).children().last().text()
     });
     // проходим по истории регистрации
@@ -114,9 +131,9 @@ const sleep = (ms) => {
         })
     });
     // получаем информационное сообщение
-    let info = await page.$eval('#checkAutoHistory .check-space.check-message', e => e.innerHTML);
-    // возвращаем результат
-    JSON.stringify(returnObject.vehicle) !== "{}" ? console.log(JSON.stringify(returnObject)) : console.log(JSON.stringify({ error: info }))
+    let info = await page.$eval('#'+id+' .check-space.check-message', e => e.innerHTML);
     // закрываем сессию
     await browser.close()
+    // возвращаем результат
+    return JSON.stringify(returnObject.vehicle) !== "{}" ? console.log(JSON.stringify(returnObject)) : console.log(JSON.stringify({ error: info }))
 })();
